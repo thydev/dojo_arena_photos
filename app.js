349b22b4e3568abfa2fa004a468a3ceb64a4ea23
Chanthy Ngin <nginchanthy@gmail.com>
2018-02-28 17:03:20 -0800

    Added Select Player option to the Select Players

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
     var isClick = false;
     //Make popup div middle the parent div
     $('.popup').css('margin-top', ($("#stage")[0].clientHeight / 2) - $(".popup")[0].clientHeight / 2);
+    //Hide the players when the page load
     $(".player1").hide();
     $(".player2").hide();
+
     $("button").hover(function() {
         //Change the background image when mouse over
         $("#stage").css("background-image", "url('" + $(this).attr("data-img") + "')");
     }, function(){
+        //Avoid the black screen appear when the user click the button
         if (!isClick)
             $("#stage").css("background-image","");
     });
@@ -23,15 +26,23 @@ $(document).ready(function(){
         $(".selectplayers").show();
         $('.selectplayers').css('margin-top', ($("#stage")[0].clientHeight / 2) - $(".selectplayers")[0].clientHeight / 2);
     });
-
-    $("#player1").change(function(){
-        $(".player1").attr("src", $(this).val());
-        $(".player1").show(500);
+ 
+    $("#player1").change(function(){ 
+        if($(this).val()==""){
+            $(".player1").hide();
+        } else {
+            $(".player1").attr("src", $(this).val());
+            $(".player1").show(2000);
+        }
     });
 
     $("#player2").change(function(){
-        $(".player2").attr("src", $(this).val());
-        $(".player2").show(500);
+        if($(this).val()==""){
+            $(".player2").hide();
+        } else {
+            $(".player2").attr("src", $(this).val());
+            $(".player2").show(2000);
+        }
     });
 
 });
