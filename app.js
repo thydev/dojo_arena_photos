0971113bd5e7a12193b0282e596bc41e759644e7
Chanthy Ngin <nginchanthy@gmail.com>
2018-02-28 16:03:25 -0800

    Complete wire frame stage 3

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ $(document).ready(function(){
     var isClick = false;
     //Make popup div middle the parent div
     $('.popup').css('margin-top', ($("#stage")[0].clientHeight / 2) - $(".popup")[0].clientHeight / 2);
-
-
-
-
-
+    $(".player1").hide();
+    $(".player2").hide();
     $("button").hover(function() {
         //Change the background image when mouse over
         $("#stage").css("background-image", "url('" + $(this).attr("data-img") + "')");
@@ -15,11 +12,6 @@ $(document).ready(function(){
         if (!isClick)
             $("#stage").css("background-image","");
     });
-
-
-
-
-
 
     $("button").click(function(){
         //Keep the background image
@@ -32,6 +24,14 @@ $(document).ready(function(){
         $('.selectplayers').css('margin-top', ($("#stage")[0].clientHeight / 2) - $(".selectplayers")[0].clientHeight / 2);
     });
 
+    $("#player1").change(function(){
+        $(".player1").attr("src", $(this).val());
+        $(".player1").show(500);
+    });
 
+    $("#player2").change(function(){
+        $(".player2").attr("src", $(this).val());
+        $(".player2").show(500);
+    });
 
 });
